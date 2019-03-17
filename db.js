756d92f4c08fc68cd1cd408172f62b3ba8c18bec
Mayk Brito <maykbrito@gmail.com>
2019-03-17 01:07:03 -0300

    mongo is ready

---
+++ db.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('Mongo is connected'))
+    .catch(err => console.log(err))
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('./db')
 
 app.use(express.json())
 
